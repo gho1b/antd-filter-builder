0472b3a8ce5cc3a9db99de755cd57dea41fcf21a
anggitgho1b <anggit@code.id>
2024-02-14 02:50:39 +0700

    Update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { resolve } from "path";
+import { resolve, relative, extname } from "path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import { glob } from "glob";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,18 +13,27 @@ export default defineConfig({
     minify: true,
     cssCodeSplit: true,
     cssMinify: true,
+    sourcemap: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "lib/index.ts"),
       name: "Ant Design Filter Builder",
       // the proper extensions will be added
-      fileName: "antd-filter-builder",
-      formats: ["es", "cjs", "umd"],
+      fileName: "index",
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["react"],
+      external: ["react", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        glob
+          .sync("./lib/**/*.{ts,tsx}")
+          .map((file) => [
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
@@ -30,8 +42,18 @@ export default defineConfig({
         globals: {
           react: "React",
         },
+        assetFileNames: "asset/[name][extname]",
+        entryFileNames: "[name].js",
+        inlineDynamicImports: false,
       },
     },
   },
-  plugins: [react(), dts({ include: ["lib"], copyDtsFiles: true })],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      include: ["lib"],
+      copyDtsFiles: true,
+    }),
+  ],
 });
