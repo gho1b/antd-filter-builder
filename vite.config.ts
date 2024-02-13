414dce0a376f1982a7269bd8a856987b51255d71
anggitgho1b <anggit@code.id>
2024-02-14 01:20:11 +0700

    Update build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    force: true,
-  },
   build: {
-    minify: false,
+    copyPublicDir: false,
+    minify: true,
+    cssCodeSplit: true,
+    cssMinify: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: resolve(__dirname, "lib/index.ts"),
       name: "Ant Design Filter Builder",
       // the proper extensions will be added
       fileName: "antd-filter-builder",
-      formats: ["es", "cjs"],
+      formats: ["es", "cjs", "umd"],
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
@@ -24,13 +25,13 @@ export default defineConfig({
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
-        preserveModules: true,
-        preserveModulesRoot: "src",
+        // preserveModules: true,
+        // preserveModulesRoot: "src",
         globals: {
           react: "React",
         },
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["lib"], copyDtsFiles: true })],
 });
